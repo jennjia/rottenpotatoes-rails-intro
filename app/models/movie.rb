646b1a2ef98cc2169aaf59f56498d19783d00de3
Jennifer Jia <jenniferjia00@berkeley.edu>
2020-10-16 07:59:40 +0000

    part 1 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,12 @@
 class Movie < ActiveRecord::Base
+  def self.all_ratings
+    return ['G', 'PG', 'PG-13', 'R']
+  end 
+  def self.with_ratings(ratings_list)
+    if ratings_list.empty?
+      self.all 
+    else
+      self.where(rating: ratings_list)
+    end 
+  end
 end
